8c263d44b07e8cb487b17f7594c48f1f72d62547
sandeepvaranasi1 <129755758+sandeepvaranasi1@users.noreply.github.com>
2024-10-11 16:05:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
-    if (x.className === "nav") {
-      x.className += " responsive";
+    if (x.className.includes("responsive")) {
+        x.className = x.className.replace(" responsive", "");
     } else {
-      x.className = "nav";
+        x.className += " responsive";
     }
-  }
+}
